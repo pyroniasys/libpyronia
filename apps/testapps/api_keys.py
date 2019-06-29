9dfbf51c7b87b7c6861ffb4f2dc0e2becb3b0041
Marcela Melara <msmelara@gmail.com>
2019-06-28 23:57:41 -0400

    Add final test apps

--- apps/testapps/api_keys.py
+++
@@ -1,5 +0,0 @@
-# Replace each with the keys and tokens from your twitter app
-consumer_key = 'y0kc7BAIZHyrJUSr71tXAPCLc'
-consumer_secret = 'L138oWRyq7KeSoVSo4fdPlqCWmJGkGP6jAEdlpWGmTJVS1ZheB'
-access_token = '987779602264285184-sl6Sym8YH9byDpJdCQLIs2Qsybq5IEh'
-access_token_secret = 'AvfVJxVyTX3boqgB3AtzHhmAzZCOhKCLRP63zIsNIAcS9'
