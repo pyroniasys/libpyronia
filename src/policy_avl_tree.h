02ccf1d832754f771d1f9a60e5e91819ec878757
Marcela Melara <msmelara@gmail.com>
2020-02-04 17:19:44 -0800

    Add license

--- src/policy_avl_tree.h
+++ src/policy_avl_tree.h
@@ -1,4 +1,18 @@
-/** Defines an AVL tree for keeping track of verified requested resources
+/* Copyright 2019 Princeton University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Defines an AVL tree for keeping track of verified requested resources
  * to support call stack caching in the kernel.
  *
  *@author Marcela S. Melara
